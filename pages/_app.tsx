e71958f9651d13114c668245a5c7c98b492541a9
Randy-RM <randymuhema@gmail.com>
2023-09-07 14:30:44 +0200

    refactor : change front-end framework

--- pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import "../dist/sass/globals.scss";
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
