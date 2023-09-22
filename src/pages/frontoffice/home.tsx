7fa9d4b7d750a96d0f6229c19c13f1da5835e5ec
Randy-RM <randymuhema@gmail.com>
2023-09-22 11:36:35 +0200

    feat : creat about section

--- src/pages/frontoffice/home.tsx
+++ src/pages/frontoffice/home.tsx
@@ -1,6 +1,8 @@
 import { Helmet } from "react-helmet";
 import { Container, Spacer } from "../../components";
 import RandyPicture from "../../assets/images/rm-hero-photo.png";
+import GithubCircleIcon from "../../assets/images/iconoir_github_circle.svg";
+import LinkedinIcon from "../../assets/images/iconoir_linkedin.svg";
 
 const Home = (): JSX.Element => {
   return (
@@ -11,7 +13,7 @@ const Home = (): JSX.Element => {
       </Helmet>
       <main className="container">
         {/**Hero section start */}
-        <div className="bg-light-grey">
+        <section className="bg-light-grey">
           <Spacer height={2} />
           <Container
             flexDirection="row"
@@ -44,15 +46,61 @@ const Home = (): JSX.Element => {
               <p>
                 <img
                   src={RandyPicture}
-                  alt="Randy Assani"
+                  alt="Randy Assani Picture"
                   className="hero-image"
                 />
               </p>
             </div>
           </Container>
-          <Spacer height={2} />
-        </div>
+          <Spacer height={4} />
+        </section>
         {/**Hero section end */}
+        {/**About me section start */}
+        <section className="bg-light-grey">
+          <Spacer height={6} />
+          <Container
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
+            flexWrap="wrap"
+          >
+            <div className="width-40 text-center-on-mobile">
+              <h2 className="font-w-Black">
+                ABOUT <span className="font-brand-color">ME</span>
+              </h2>
+              <p>
+                <span className="font-w-extra-bold">FOLLOW ME ON</span>
+                <br />
+                <a href="https://github.com/Randy-RM" target="_blank">
+                  <img src={GithubCircleIcon} alt="Github profile" />
+                </a>
+                &nbsp;
+                <a
+                  href="https://www.linkedin.com/in/randy-assani-beni-ab101216b/"
+                  target="_blank"
+                >
+                  <img src={LinkedinIcon} alt="Linkdin profile" />
+                </a>
+              </p>
+            </div>
+            <div className="width-50 p-2 bg-mid-grey text-center-on-mobile">
+              <p>HI. I'm Randy Assani Beni (RM).</p>
+              <p>
+                I'm Analyst Designer with a Computer Science degree and
+                experience on computer engineering, software development &
+                graphic design.
+              </p>
+              <p>
+                I am always developing my skills and learning something new. I
+                have a thirst for knowledge and a desire to understand how
+                things work; I'm a people person, a tinkerer, a lover of
+                technology/science."
+              </p>
+            </div>
+          </Container>
+          <Spacer height={6} />
+        </section>
+        {/**About me section end */}
       </main>
     </>
   );
