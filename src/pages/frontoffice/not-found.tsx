e95230eecbe81210c2658f8f68986126fa37390d
Randy-RM <randymuhema@gmail.com>
2023-09-29 16:58:22 +0200

    feat : creat 404 page

--- src/pages/frontoffice/not-found.tsx
+++ src/pages/frontoffice/not-found.tsx
@@ -1,4 +1,7 @@
 import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
+import { Container, Spacer } from "../../components";
+import EmojiPuzzled from "../../assets/images/iconoir_emoji_puzzled.svg";
 
 const NotFound = (): JSX.Element => {
   return (
@@ -7,7 +10,37 @@ const NotFound = (): JSX.Element => {
         <meta charSet="utf-8" />
         <title>404 | Randy Assani Portfolio</title>
       </Helmet>
-      <h1>404 : Page Not Found</h1>
+      <main>
+        <section className="bg-light-grey">
+          <div className="container">
+            <Spacer height={6} />
+            <Container
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+              flexWrap="wrap"
+            >
+              <p className="text-center">
+                <img
+                  src={EmojiPuzzled}
+                  className="logo"
+                  alt="RM Randy Assani Logo"
+                />
+                <br />
+                <span className="font-big-hero font-w-Black">
+                  4<span className="font-brand-color">0</span>4
+                </span>
+                <br />
+                <span>Page Not Found</span>
+              </p>
+              <p className="text-center">
+                <Link to="/">Click here to go back to home page</Link>
+              </p>
+            </Container>
+            <Spacer />
+          </div>
+        </section>
+      </main>
     </>
   );
 };
