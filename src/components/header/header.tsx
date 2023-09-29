8f21d740ce42dd71812db6daefb129ce4256b08e
Randy-RM <randymuhema@gmail.com>
2023-09-29 15:32:05 +0200

    feat : add link to home page on header logo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,25 +1,15 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import LogoRM from "../../assets/images/logo-rm.svg";
 
 const Header = (): JSX.Element => {
-  // return (
-  //   <header>
-  //     <nav style={{ margin: 10 }}>
-  //       <Link to="/" style={{ padding: 5 }}>
-  //         Home
-  //       </Link>
-  //       <Link to="/projects" style={{ padding: 5 }}>
-  //         My projects
-  //       </Link>
-  //     </nav>
-  //   </header>
-  // );
   return (
     <header className="bg-light-grey">
       <div className="container">
         <nav className="nav-bar">
           <div className="">
-            <img src={LogoRM} className="logo" />
+            <Link to="/">
+              <img src={LogoRM} className="logo" />
+            </Link>
           </div>
           <div className="">
             <label htmlFor="check">
