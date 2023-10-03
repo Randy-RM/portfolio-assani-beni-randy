52d0cd021a80a33baea5dc2185ec52347e151e7c
Randy-RM <randymuhema@gmail.com>
2023-10-03 15:15:23 +0200

    feat : add motion animation 404 page

--- src/pages/frontoffice/not-found.tsx
+++ src/pages/frontoffice/not-found.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import { Container, Spacer } from "../../components";
 import EmojiPuzzled from "../../assets/images/iconoir_emoji_puzzled.svg";
 
@@ -14,29 +15,41 @@ const NotFound = (): JSX.Element => {
         <section className="bg-light-grey">
           <div className="container">
             <Spacer height={6} />
-            <Container
-              flexDirection="column"
-              justifyContent="center"
-              alignItems="center"
-              flexWrap="wrap"
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              viewport={{ once: true }}
+              transition={{
+                type: "spring",
+                delay: 0.3,
+                easeInOut: "linear",
+                duration: 2,
+              }}
             >
-              <p className="text-center">
-                <img
-                  src={EmojiPuzzled}
-                  className="logo"
-                  alt="RM Randy Assani Logo"
-                />
-                <br />
-                <span className="font-big-hero font-w-Black">
-                  4<span className="font-brand-color">0</span>4
-                </span>
-                <br />
-                <span>Page Not Found</span>
-              </p>
-              <p className="text-center">
-                <Link to="/">Click here to go back home page</Link>
-              </p>
-            </Container>
+              <Container
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+                flexWrap="wrap"
+              >
+                <p className="text-center">
+                  <img
+                    src={EmojiPuzzled}
+                    className="logo"
+                    alt="RM Randy Assani Logo"
+                  />
+                  <br />
+                  <span className="font-big-hero font-w-Black">
+                    4<span className="font-brand-color">0</span>4
+                  </span>
+                  <br />
+                  <span>Page Not Found</span>
+                </p>
+                <p className="text-center">
+                  <Link to="/">Click here to go back home page</Link>
+                </p>
+              </Container>
+            </motion.div>
             <Spacer />
           </div>
         </section>
