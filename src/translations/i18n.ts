fe45a981e3279e528e4750fe5a739247fd33a1ca
Randy-RM <randymuhema@gmail.com>
2023-12-11 14:23:59 +0100

    feat : add i18n translation configuration

---
+++ src/translations/i18n.ts
@@ -0,0 +1,39 @@
+import i18n from "i18next";
+import detector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+import global_en from "./en/global.json";
+import global_fr from "./fr/global.json";
+
+const resources = {
+  en: {
+    common: global_en,
+  },
+  fr: {
+    common: global_fr,
+  },
+};
+
+const options = {
+  order: ["navigator"],
+  // lookupQuerystring: "lng",
+};
+
+i18n
+  .use(detector) // Registering the detection plugin
+  .use(initReactI18next)
+  .init({
+    debug: true,
+    // language resources
+    resources,
+    detection: options,
+    fallbackLng: "en",
+    ns: ["common"],
+    defaultNS: "common",
+    supportedLngs: ["en", "fr"],
+    nonExplicitSupportedLngs: true,
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+  });
+
+export default i18n;
