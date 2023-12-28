95def266b15ffc9c6c71a6ddc466d4ec39338f9b
Randy-RM <randymuhema@gmail.com>
2023-12-28 15:34:48 +0100

    feat : creat grouped import for themed icons components

---
+++ src/assets/images/index.tsx
@@ -0,0 +1,7 @@
+export { default as LogoRm } from "./logo-rm";
+export { default as GithubIcon } from "./github-icon";
+export { default as LinkedinIcon } from "./linkedin-icon";
+export { default as ToolsIcon } from "./tools-icon";
+export { default as DesignPencilIcon } from "./design-pencil-icon";
+export { default as LeaderboardStarIcon } from "./leaderboard-star-icon";
+export { default as EmojiPuzzledIcon } from "./emoji-puzzled-icon";
