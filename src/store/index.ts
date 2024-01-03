a6edb1c138e642a5708f762016ee9d9b4d2753b3
Randy-RM <randymuhema@gmail.com>
2024-01-03 10:15:30 +0100

    refactor : add theme store on exported store object

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,2 +1,3 @@
 export { default as useModalStatusStore } from "./modal-status-store";
 export { default as useMailPerDayStore } from "./mails-per-day-store";
+export { default as useThemeStore } from "./theme-store";
