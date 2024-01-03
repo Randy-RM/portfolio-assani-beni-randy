224b8095707b02193aaf9a66435dd302d235fd67
Randy-RM <randymuhema@gmail.com>
2024-01-03 15:35:03 +0100

    feat : add translated greetings on loader page

--- src/components/loader/loader.tsx
+++ src/components/loader/loader.tsx
@@ -1,9 +1,16 @@
+import { useTranslation } from "react-i18next";
+
 const Loader = (): JSX.Element => {
+  const { t } = useTranslation();
+
   return (
-    <main className="starting-loader-page">
+    <main className="starting-loader-page font-secondary-color">
       <div>
         <span className="starting-loader"></span>
       </div>
+      <p className="text-center font-w-extra-thin font-big-hero">
+        {t(`greetings`)}
+      </p>
     </main>
   );
 };
